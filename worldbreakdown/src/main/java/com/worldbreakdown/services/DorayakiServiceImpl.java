174d91cedbd290eef59d2b4576eb35dabbc7c9c5
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-11-25 00:02:34 +0700

    fix return

--- worldbreakdown/src/main/java/com/worldbreakdown/services/DorayakiServiceImpl.java
+++ worldbreakdown/src/main/java/com/worldbreakdown/services/DorayakiServiceImpl.java
@@ -86,6 +86,31 @@ public class DorayakiServiceImpl implements DorayakiService {
 
     @Override
     public String getRequestStock() {
-        return "Dorayaki";
+        String result = "";
+        try {
+
+            URL url = new URL("http://localhost:5000/api/requests?limit=true");// your url i.e fetch
+                                                                                                  // data from .
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode());
+            }
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output = "";
+            while ((output = br.readLine()) != null) {
+                result = result + output;
+            }
+            conn.disconnect();
+            JSONObject obj = new JSONObject(result);
+            result = obj.getString("data");
+
+        } catch (Exception e) {
+            System.out.println("Exception in NetClientGet:- " + e);
+        }
+
+        return result;
     }
 }
